fd9d28ffab4b66d7ac8ddd2ebf8ce7ed48cdd9be
magava <magaava@gmail.com>
2016-02-15 02:02:52 +0400

    Testing Console Application

---
+++ TestingHelloWorldRepo/TestingHelloWorldRepo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingHelloWorldRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, from Visual Studio!!!");
+        }
+    }
+}
